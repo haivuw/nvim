1fb51f08c458e69fd2c3c06b2f012506776fc7f8
haivuw <haivuw@gmail.com>
2024-04-14 15:42:06 +0700

    plugs

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,15 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "echasnovski/mini.files",
+    keys = {
+      {
+        "<C-e>",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
+      },
+    },
+  },
+}
