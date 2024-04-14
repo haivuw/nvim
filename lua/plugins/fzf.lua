39fc14bed6735b4e8de9ffd584b6e4fd2eb7062c
haivuw <haivuw@gmail.com>
2024-04-14 09:51:04 +0700

    fzf

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,26 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    -- calling `setup` is optional for customization
+    require("fzf-lua").setup({})
+  end,
+  keys = {
+    {
+      "<C-p>",
+      function()
+        require("fzf-lua").files()
+      end,
+      { silent = true },
+    },
+    {
+      "<leader>gs",
+      function()
+        require("fzf-lua").git_status()
+      end,
+      { silent = true },
+      desc = "status",
+    },
+  },
+}
