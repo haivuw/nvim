38037abf273aa7283ba46549027237cf292a5126
haivuw <haivuw@gmail.com>
2024-04-20 08:04:56 +0700

    comment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Don't auto insert comments on newline
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    vim.opt.formatoptions:remove({ "o" })
+  end,
+})
