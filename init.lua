407d22367810c8593f0c9f83f7f7c20cf84261b6
haivuw <haivuw@gmail.com>
2026-01-31 05:26:37 +0700

    .

--- init.lua
+++ init.lua
@@ -175,47 +175,6 @@ require("lazy").setup({
       end,
     },
     {
-      "Exafunction/codeium.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-      },
-      config = function()
-        require("codeium").setup({
-
-          -- Optionally disable cmp source if using virtual text only
-          enable_cmp_source = false,
-          virtual_text = {
-            enabled = true,
-
-            -- How long to wait (in ms) before requesting completions after typing stops.
-            idle_delay = 75,
-            -- Priority of the virtual text. This usually ensures that the completions appear on top of
-            -- other plugins that also add virtual text, such as LSP inlay hints, but can be modified if
-            -- desired.
-            virtual_text_priority = 65535,
-            -- Set to false to disable all key bindings for managing completions.
-            map_keys = true,
-            -- Key bindings for managing completions in virtual text mode.
-            key_bindings = {
-              -- Accept the current completion.
-              accept = "<Tab>",
-              -- Accept the next word.
-              accept_word = false,
-              -- Accept the next line.
-              accept_line = false,
-              -- Clear the virtual text.
-              clear = false,
-              -- Cycle to the next completion.
-              next = "<M-]>",
-              -- Cycle to the previous completion.
-              prev = "<M-[>",
-            },
-          },
-        })
-      end,
-    },
-    {
       "folke/ts-comments.nvim",
       opts = {},
       event = "VeryLazy",
